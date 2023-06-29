655f7f98a5b425ffe50c0d4848889211f0da1a5e
Pratham Byanjankar <overlord.damygod@gmail.com>
2023-06-29 14:00:01 +0545

    Added Github Action to deploy to Azure App Services (#1)
    * add: workflow for azure
    
    * fix: workflow
    
    * update: removed email confirmation

--- PSPCommerce/Program.cs
+++ PSPCommerce/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<UserManager<User>>();
